feb3a7224f51b40a4ead979d4c2e2a155f1e410f
angela <adeng32@uwo.ca>
2023-11-25 10:24:33 -0500

    Updated file structure.

--- in-english-please/src/index.jsx
+++ in-english-please/src/index.jsx
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 
 import App from "./app.jsx";
-import "./index.css";
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />);
