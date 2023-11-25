8b9da08ca443b350c29e52e0a3855601d7aa2e06
angela <adeng32@uwo.ca>
2023-11-25 15:11:23 -0500

    Updated CSS.

--- english-please/src/components/Button.jsx
+++ english-please/src/components/Button.jsx
@@ -1,18 +1,12 @@
 // import statements
 import './Button.css';
 import { Link } from 'react-router-dom';
-import ingrLabel from '../images/ingredientLabel.jpg'
-import {useRef, useState } from "react";
+import {useRef } from "react";
 
 // component
 export default function Button(){
     // this initializes a React ref, this is used to reference input element
     const inputRef = useRef(null);
-
-    // the useState hook initializes the state variable
-    // image captures the current state of the image, setImage is the function that manages the state
-    const [image, setImage] = useState("")
-
     // a function that gets involved whenever the user clicks on the upload button
     // whenever the user clicks on the current inputRef
     const handleImageClick = () => {
@@ -31,9 +25,12 @@ export default function Button(){
         <div>
             <div id="uploadBtn" onClick={handleImageClick}>
                 <input type="file" ref={inputRef} onChange={handleImageChange} style = {{display: "none"}}></input>
-                <button>Upload Your Image!</button>
+                <button onClick = {() => {
+                    // insert connection to backend
+                    
+                }}>Upload Your Image!</button>
             </div>
-            <Link to="/informationPage"><button>CLICK ON ME</button></Link>
+            <Link to="/informationPage"><button>Upload File!</button></Link>
         </div>
     )
 }
